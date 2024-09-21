4eefed56ff5fefd94d1999266a653f6e55bb31b6
Ashraf-Khaled-2005 <ashrafkhaled200514@gmail.com>
2024-09-22 01:18:54 +0300

    create Structure of APp

---
+++ lib/Features/Auth/view/SignUpScreen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'widget/SignUpScreenBody.dart';
+
+class Signupscreen extends StatelessWidget {
+  const Signupscreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Signupscreenbody(),
+    );
+  }
+}
---
+++ lib/Features/Auth/view/widget/SignUpScreenBody.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Signupscreenbody extends StatelessWidget {
+  const Signupscreenbody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
