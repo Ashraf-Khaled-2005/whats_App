fec8a0e5a97c7ec15855a85fe024f4e85f79d8ad
Ashraf-Khaled-2005 <ashrafkhaled200514@gmail.com>
2024-10-16 23:12:19 +0300

    create Search logic in chat and add contact

--- lib/Features/Chats/presentation/view/Chat.dart
+++ lib/Features/Chats/presentation/view/Chat.dart
@@ -1,5 +1,4 @@
 import 'dart:developer';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:our_whatsapp/Features/Chats/presentation/view/widget/addcontact.dart';
@@ -13,6 +12,7 @@ class ChatScreen extends StatefulWidget {
   final MyUserData user;
 
   const ChatScreen({super.key, required this.user});
+
   @override
   _ChatScreenState createState() => _ChatScreenState();
 }
@@ -25,15 +25,16 @@ class _ChatScreenState extends State<ChatScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: IconButton(
-          onPressed: () async {
-            await Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => Addcontact(),
-              ),
-            );
-          },
-          icon: const Icon(Icons.add)),
+        onPressed: () async {
+          await Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => Addcontact(),
+            ),
+          );
+        },
+        icon: const Icon(Icons.add),
+      ),
       appBar: AppBar(
         title: _isSearching
             ? TextField(
@@ -90,69 +91,83 @@ class _ChatScreenState extends State<ChatScreen> {
         leading: null,
       ),
       body: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-          stream: FirebaseFirestore.instance
-              .collection('Chats')
-              .where('participants', arrayContains: widget.user.id)
-              .orderBy('time', descending: true)
-              .snapshots(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: CircularProgressIndicator());
-            } else if (snapshot.hasError) {
-              log(snapshot.error.toString());
-              return Center(child: Text('Error: ${snapshot.error}'));
-            } else if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-              return const Center(child: Text('No chats found.'));
+        stream: FirebaseFirestore.instance
+            .collection('Chats')
+            .where('participants', arrayContains: widget.user.id)
+            .orderBy('time', descending: true)
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            log(snapshot.error.toString());
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+            return const Center(child: Text('No chats found.'));
+          }
+
+          var filteredUsers = snapshot.data!.docs.where((doc) {
+            var chatData = doc.data();
+            String username;
+
+            if (chatData['senderid'] == widget.user.id) {
+              username = chatData['receivername']?.toLowerCase() ?? '';
+            } else {
+              username = chatData['sendername']?.toLowerCase() ?? '';
             }
-            var result = snapshot.data!.docs;
 
-            return ListView.builder(
-              itemCount: result.length,
-              itemBuilder: (context, index) {
-                return ListTileItem(
-                  name: result[index]['senderid'] == widget.user.id
-                      ? result[index]['receivername']
-                      : result[index]['sendername'],
-                  message: result[index]['lastmessage'] ?? "",
-                  time:
-                      (result[index]['time'] as Timestamp).toDate().toString(),
-                  imageUrl: result[index]['senderid'] == widget.user.id
-                      ? result[index]['receiverimage']
-                      : result[index]['senderimage'],
-                  onTapProfilePicture: () {
-                    showProfilePictureDialog(
-                        context,
-                        result[index]['senderid'] == widget.user.id
-                            ? result[index]['receiverimage']
-                            : result[index]['senderimage']);
-                  },
-                  onTapChatItem: () async {
-                    DocumentSnapshot<Map<String, dynamic>> data;
-                    if (result[index]['senderid'] == widget.user.id) {
-                      data = await FirebaseFirestore.instance
-                          .collection('Users')
-                          .doc(result[index]['receiverid'])
-                          .get();
-                    } else {
-                      data = await FirebaseFirestore.instance
-                          .collection('Users')
-                          .doc(result[index]['senderid'])
-                          .get();
-                    }
-                    var nextuser = data.data();
-                    MyUserData userData = MyUserData.fromJson(nextuser!);
+            return username.contains(searchQuery.toLowerCase());
+          }).toList();
 
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) =>
-                              ChatDetailScreen(user: userData),
-                        ));
-                  },
-                );
-              },
-            );
-          }),
+          return ListView.builder(
+            itemCount: filteredUsers.length,
+            itemBuilder: (context, index) {
+              var chat = filteredUsers[index].data();
+              return ListTileItem(
+                name: chat['senderid'] == widget.user.id
+                    ? chat['receivername']
+                    : chat['sendername'],
+                message: chat['lastmessage'] ?? "",
+                time: (chat['time'] as Timestamp).toDate().toString(),
+                imageUrl: chat['senderid'] == widget.user.id
+                    ? chat['receiverimage']
+                    : chat['senderimage'],
+                onTapProfilePicture: () {
+                  showProfilePictureDialog(
+                    context,
+                    chat['senderid'] == widget.user.id
+                        ? chat['receiverimage']
+                        : chat['senderimage'],
+                  );
+                },
+                onTapChatItem: () async {
+                  DocumentSnapshot<Map<String, dynamic>> data;
+                  if (chat['senderid'] == widget.user.id) {
+                    data = await FirebaseFirestore.instance
+                        .collection('Users')
+                        .doc(chat['receiverid'])
+                        .get();
+                  } else {
+                    data = await FirebaseFirestore.instance
+                        .collection('Users')
+                        .doc(chat['senderid'])
+                        .get();
+                  }
+                  var nextuser = data.data();
+                  MyUserData userData = MyUserData.fromJson(nextuser!);
+
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => ChatDetailScreen(user: userData),
+                    ),
+                  );
+                },
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
--- lib/Features/Chats/presentation/view/widget/addcontact.dart
+++ lib/Features/Chats/presentation/view/widget/addcontact.dart
@@ -1,86 +1,80 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_advanced_avatar/flutter_advanced_avatar.dart';
 import 'package:our_whatsapp/Features/Chats/data/model/userData.dart';
-
 import 'ChatItem.dart';
 import 'Chat_Detail.dart';
 
-class Addcontact extends StatelessWidget {
-  Addcontact({super.key});
-  List New = const [
-    {
-      "image":
-          "https://cdn.stocksnap.io/img-thumbs/960w/mountains-clouds_A4Y2Q66EL4.jpg",
-      "name": "New group",
-    },
-    {
-      "image":
-          "https://cdn.create.vista.com/api/media/small/8099055/stock-photo-autumnal-natural-background",
-      "name": "New contact",
-    },
-    {
-      "image":
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiYdLgEeNF0dmf4tC2me4zMZpEQBVJwyv6ta6T1qgsrg&s",
-      "name": "New comuntiy",
-    }
-  ];
-  List chats = [
-    {
-      "image":
-          "https://cdn.stocksnap.io/img-thumbs/960w/mountains-clouds_A4Y2Q66EL4.jpg",
-      "name": "(you)"
-    },
-    {
-      "image":
-          "https://cdn.create.vista.com/api/media/small/8099055/stock-photo-autumnal-natural-background",
-      "name": "01256374527"
-    },
-    {
-      "image":
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiYdLgEeNF0dmf4tC2me4zMZpEQBVJwyv6ta6T1qgsrg&s",
-      "name": "012e6364263"
-    }
-  ];
+class Addcontact extends StatefulWidget {
+  const Addcontact({super.key});
+
+  @override
+  State<Addcontact> createState() => _AddcontactState();
+}
+
+class _AddcontactState extends State<Addcontact> {
+  String searchQuery = "";
+  bool _isSearching = false;
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        appBar: AppBar(
+          title: _isSearching
+              ? TextField(
+                  decoration: const InputDecoration(
+                    hintText: 'Search',
+                    border: InputBorder.none,
+                    hintStyle: TextStyle(color: Colors.white54),
+                  ),
+                  style: const TextStyle(color: Colors.white),
+                  onChanged: (value) {
+                    setState(() {
+                      searchQuery = value;
+                    });
+                  },
+                )
+              : const Text("Start Chatting",
+                  style: TextStyle(color: Colors.white)),
+          backgroundColor: Colors.green,
+          actions: [
+            if (!_isSearching) ...[
+              IconButton(
+                icon: const Icon(Icons.search),
+                onPressed: () {
+                  setState(() {
+                    _isSearching = true;
+                    searchQuery = "";
+                  });
+                },
+              ),
+            ] else ...[
+              IconButton(
+                icon: const Icon(Icons.close),
+                onPressed: () {
+                  setState(() {
+                    _isSearching = false;
+                    searchQuery = "";
+                  });
+                },
+              ),
+            ],
+          ],
+          leading: null,
+        ),
         body: Column(
           children: [
-            Row(
-              children: [
-                IconButton(
-                  onPressed: () => Navigator.pop(context),
-                  icon: const Icon(Icons.arrow_back),
-                ),
-                const Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text(
-                    'Select contact',
-                    style: const TextStyle(fontSize: 13),
-                  ),
-                ),
-                const Spacer(),
-                const Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Icon(Icons.search),
-                ),
-              ],
-            ),
             const SizedBox(height: 30),
             StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
               stream:
                   FirebaseFirestore.instance.collection('Users').snapshots(),
               builder: (context, snapshot) {
-                // Handle error state
                 if (snapshot.hasError) {
                   return Center(
                     child: Text('Something went wrong! ${snapshot.error}'),
                   );
                 }
 
-                // Handle loading state
                 if (snapshot.connectionState == ConnectionState.waiting) {
                   return const Center(child: CircularProgressIndicator());
                 }
@@ -91,7 +85,14 @@ class Addcontact extends StatelessWidget {
                     child: Text('No chats available'),
                   );
                 }
+                var filteredUsers = snapshot.data!.docs.where((doc) {
+                  var chatData = doc.data();
+                  var username = chatData['username']?.toLowerCase() ?? '';
+                  var mobile = chatData['phone']?.toLowerCase() ?? '';
 
+                  return username.contains(searchQuery) ||
+                      mobile.contains(searchQuery); // Filter based on search
+                }).toList();
                 return Expanded(
                   child: Column(
                     children: [
@@ -114,9 +115,9 @@ class Addcontact extends StatelessWidget {
                               color: Colors.grey,
                             ),
                           ),
-                          itemCount: snapshot.data!.docs.length,
+                          itemCount: filteredUsers.length,
                           itemBuilder: (context, index) {
-                            var chatData = snapshot.data!.docs[index].data();
+                            var chatData = filteredUsers[index].data();
                             MyUserData user = MyUserData(
                                 image: chatData['image'],
                                 username: chatData['username'],
